4f51023c755ee4a22873709812f5e9fe928cc73d
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-21 14:49:29 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,16 +1,16 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
   # add the code snippet here!
   winner=""
-  passengers.each{ |suit, name| 
-    if 
+  passengers.each{ |suit, name|
+    if
       suit=="suite_a" && name.start_with?("A")
     end
   }
